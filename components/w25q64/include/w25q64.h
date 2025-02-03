4fa964906889e029c35c726390e60077de618922
Heptari <heptari@outlook.com>
2025-02-03 12:50:25 +0800

    Add W25Q64 features.

--- components/w25q64/include/w25q64.h
+++ components/w25q64/include/w25q64.h
@@ -1,1 +1,16 @@
-void func(void);
+#ifndef __W25Q64_H
+#define __W25Q64_h
+
+#include <stdio.h>
+
+#include "spi_basic.h"
+#include "w25q64_ins.h"
+
+typedef struct {
+  uint8_t mosi_pin, miso_pin, clk_pin, cs_pin;
+} W25Q64_t;
+
+void W25Q64_init(W25Q64_t *inf);
+void W25Q64_readID(uint8_t *MID, uint8_t *DID);
+
+#endif  // !__W25Q64_H
--- components/w25q64/w25q64.c
+++ components/w25q64/w25q64.c
@@ -1,7 +1,39 @@
-#include <stdio.h>
 #include "w25q64.h"
 
-void func(void)
-{
+void W25Q64_init(W25Q64_t *inf) {
+  spi_basic_init(inf->miso_pin, inf->mosi_pin, inf->cs_pin, inf->clk_pin);
+}
 
+void W25Q64_readID(uint8_t *MID, uint8_t *DID) {
+  uint8_t data[4];
+  data[0] = W25Q64_JEDEC_ID;
+
+  spi_transfer(data, data, 4);
+
+  *MID = data[1];
+  *DID = data[2];
+  *DID <<= 8;
+  *DID |= data[3];
+}
+
+void W25Q64_WriteEnable(void) {
+  uint8_t data[1] = {W25Q64_WRITE_ENABLE};
+  spi_send(data, 1);
+}
+
+void W25Q64_WaitBusy(void) {
+  uint32_t timeout = 10000;
+
+  uint8_t data[2];
+  data[0] = W25Q64_READ_STATUS_REGISTER_1;
+
+  for (;;) {
+    spi_transfer(data, data, 2);
+    if ((data[1] & 0x01) == 0x01) {
+      --timeout;
+      if (timeout == 0) break;
+    } else {
+      break;
+    }
+  }
 }
